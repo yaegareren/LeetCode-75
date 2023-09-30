b62e8d2a5685c93ee49c811128c7ad0d1034f28e
Shree Mangalam Singh <sshreemangalam@gmail.com>
2023-09-30 19:25:55 +0530

    added Problem4.cpp

---
+++ Problem4.cpp
@@ -0,0 +1,25 @@
+// LC - 605
+// You have a long flowerbed in which some of the plots are planted, and some are not. However, flowers cannot be planted in adjacent plots. Given an integer array flowerbed containing 0's and 1's, where 0 means empty and 1 means not empty, and an integer n, return true if n new flowers can be planted in the flowerbed without violating the no-adjacent-flowers rule and false otherwise.
+
+class Solution {
+public:
+    bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+        int l = flowerbed.size(); 
+
+        if(n == 0) return true; // 0 flowers remaining
+
+        for(int i=0; i<l; i++){
+            if(flowerbed[i] == 0){
+                bool leftEmpty = ((flowerbed[i-1] == 0)|| i== 0); 
+                bool rightEmpty = ((flowerbed[i+1] == 0)|| i == l-1);
+
+            if(leftEmpty && rightEmpty){
+                flowerbed[i] = 1; // flower planted
+                n--; 
+            }
+            if(n == 0) return true;
+            }
+        }
+        return false; 
+    }
+};
