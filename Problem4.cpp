6f598f15b1d5387e3b2c626d9bcec6f4e48434fe
Shree Mangalam Singh <sshreemangalam@gmail.com>
2023-09-30 19:28:23 +0530

    Update Problem4.cpp

--- Problem4.cpp
+++ Problem4.cpp
@@ -10,8 +10,8 @@ public:
 
         for(int i=0; i<l; i++){
             if(flowerbed[i] == 0){
-                bool leftEmpty = ((flowerbed[i-1] == 0)|| i== 0); 
-                bool rightEmpty = ((flowerbed[i+1] == 0)|| i == l-1);
+                bool leftEmpty = (i== 0) || (flowerbed[i-1] == 0); 
+                bool rightEmpty = (i == l-1) || (flowerbed[i+1] == 0);
 
             if(leftEmpty && rightEmpty){
                 flowerbed[i] = 1; // flower planted
